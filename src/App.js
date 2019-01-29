0bf5f029dfd28cadf797f3b2d4e2e32d57b4ca60
Camilo Acuña <camilo.aku@gmail.com>
2019-01-29 10:06:53 -0300

    Add base files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Provider} from 'react-redux'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Worldmap from './screens/worldmap/worldmap.container'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = ({store}) => (
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path="/" component={Worldmap} />
       </div>
-    );
-  }
+    </Router>
+  </Provider>
+)
+
+App.propTypes = {
+  store: PropTypes.object.isRequired
 }
 
-export default App;
+export default App
